f59056848676295c61763e312abea8e78be4b72e
8nyaha8 <35270887+8nyaha8@users.noreply.github.com>
2018-05-27 13:14:21 +0300

    Testcoin: Branding

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -4,23 +4,23 @@ TOPDIR=${TOPDIR:-$(git rev-parse --show-toplevel)}
 SRCDIR=${SRCDIR:-$TOPDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-LITECOIND=${LITECOIND:-$SRCDIR/litecoind}
-LITECOINCLI=${LITECOINCLI:-$SRCDIR/litecoin-cli}
-LITECOINTX=${LITECOINTX:-$SRCDIR/litecoin-tx}
-LITECOINQT=${LITECOINQT:-$SRCDIR/qt/litecoin-qt}
+TESTCOIND=${TESTCOIND:-$SRCDIR/testcoind}
+TESTCOINCLI=${TESTCOINCLI:-$SRCDIR/testcoin-cli}
+TESTCOINTX=${TESTCOINTX:-$SRCDIR/testcoin-tx}
+TESTCOINQT=${TESTCOINQT:-$SRCDIR/qt/testcoin-qt}
 
-[ ! -x $LITECOIND ] && echo "$LITECOIND not found or not executable." && exit 1
+[ ! -x $TESTCOIND ] && echo "$TESTCOIND not found or not executable." && exit 1
 
 # The autodetected version git tag can screw up manpage output a little bit
-LTCVER=($($LITECOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
+LTCVER=($($TESTCOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
 
 # Create a footer file with copyright content.
 # This gets autodetected fine for bitcoind if --version-string is not set,
 # but has different outcomes for bitcoin-qt and bitcoin-cli.
 echo "[COPYRIGHT]" > footer.h2m
-$LITECOIND --version | sed -n '1!p' >> footer.h2m
+$TESTCOIND --version | sed -n '1!p' >> footer.h2m
 
-for cmd in $LITECOIND $LITECOINCLI $LITECOINTX $LITECOINQT; do
+for cmd in $TESTCOIND $TESTCOINCLI $TESTCOINTX $TESTCOINQT; do
   cmdname="${cmd##*/}"
   help2man -N --version-string=${LTCVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
   sed -i "s/\\\-${LTCVER[1]}//g" ${MANDIR}/${cmdname}.1
